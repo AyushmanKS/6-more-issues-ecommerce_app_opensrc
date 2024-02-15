9b0245cb42a38828813d3978d30739c3ebd20612
Jaiditya Batra <jaiditya7318@gmail.com>
2024-02-15 23:01:50 +0530

    Initial commit

---
+++ lib/data/dummy_products.dart
@@ -0,0 +1,29 @@
+import 'package:ecommerce_app_opensrc/models/product_model.dart';
+
+List<Product> dummyProducts = [
+  Product(
+    name: 'Product 1',
+    price: 10.99,
+    description: "Lorem ipsum dolor.",
+  ),
+  Product(
+    name: 'Product 2',
+    price: 20.99,
+    description: "Lorem ipsum dolor.",
+  ),
+  Product(
+    name: 'Product 3',
+    price: 30.99,
+    description: "Lorem ipsum dolor.",
+  ),
+  Product(
+    name: 'Product 4',
+    price: 20.99,
+    description: "Lorem ipsum dolor.",
+  ),
+  Product(
+    name: 'Product 5',
+    price: 30.99,
+    description: "Lorem ipsum dolor.",
+  ),
+];
---
+++ lib/models/user_model.dart
@@ -0,0 +1,8 @@
+class User {
+  final String email;
+  final String username;
+  final String phoneNumber;
+
+  User(
+      {required this.email, required this.username, required this.phoneNumber});
+}
